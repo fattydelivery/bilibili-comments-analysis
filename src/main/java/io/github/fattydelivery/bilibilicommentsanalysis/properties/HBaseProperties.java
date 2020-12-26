46332248147d1f58d7f5c13eca808eb907170543
beiyuouo <beiyu233@gmail.com>
2020-12-26 13:09:27 +0800

    add hbase properities

--- src/main/java/io/github/fattydelivery/bilibilicommentsanalysis/properties/HBaseProperties.java
+++ src/main/java/io/github/fattydelivery/bilibilicommentsanalysis/properties/HBaseProperties.java
@@ -1,5 +1,9 @@
 package io.github.fattydelivery.bilibilicommentsanalysis.properties;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 /**
  * @program:bilibili-comments-analysis
  * @description
@@ -7,5 +11,35 @@ package io.github.fattydelivery.bilibilicommentsanalysis.properties;
  * @create: 2020-12-25-15:27
  **/
 public class HBaseProperties {
+    private String ip, port;
+    private String filePath = "/cluster.properties";
 
+    public HBaseProperties() {
+        Properties properties = new Properties();
+        try {
+            InputStream in = TempFileProperties.class.getClass().getResourceAsStream(this.filePath);
+            properties.load(in);
+
+            this.ip = properties.getProperty("hbase.ip");
+            this.port = properties.getProperty("hbase.port");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getPort() {
+        return port;
+    }
+
+    public void setPort(String port) {
+        this.port = port;
+    }
 }
--- src/main/java/io/github/fattydelivery/bilibilicommentsanalysis/utils/hbase2java/HbaseConnection.java
+++ src/main/java/io/github/fattydelivery/bilibilicommentsanalysis/utils/hbase2java/HbaseConnection.java
@@ -1,5 +1,6 @@
 package io.github.fattydelivery.bilibilicommentsanalysis.utils.hbase2java;
 
+import io.github.fattydelivery.bilibilicommentsanalysis.properties.HBaseProperties;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.Cell;
 import org.apache.hadoop.hbase.HBaseConfiguration;
@@ -29,8 +30,10 @@ public class HbaseConnection {
         List<Object> list = new ArrayList<Object>();
         //注意。这里这行目前没有注释掉的，这行和问题3有关系  是要根据自己zookeeper.znode.parent的配置信息进行修改。
 //        configuration.set("zookeeper.znode.parent","/hbase-unsecure"); //与 hbase-site-xml里面的配置信息 zookeeper.znode.parent 一致
-        configuration.set("hbase.zookeeper.quorum", "hadoop001");  //hbase 服务地址
-        configuration.set("hbase.zookeeper.property.clientPort", "2181"); //端口号
+
+        HBaseProperties hbaseProperties = new HBaseProperties();
+        configuration.set("hbase.zookeeper.quorum", hbaseProperties.getIp());  //hbase 服务地址
+        configuration.set("hbase.zookeeper.property.clientPort", hbaseProperties.getPort()); //端口号
         //这里使用的是接口Admin   该接口有一个实现类HBaseAdmin   也可以直接使用这个实现类
         // HBaseAdmin baseAdmin = new HBaseAdmin(configuration);
         con = ConnectionFactory.createConnection(configuration);
